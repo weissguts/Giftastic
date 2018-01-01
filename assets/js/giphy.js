bd79c084225b9a33d5c689259ce6a69eab5ea977
Daniel Hernandez <weissguts@gmail.com>
2017-12-31 18:11:54 -0700

    Updated JS. Giphy API working. Id's now set properly. When clicked, JS grabs proper ID. Need to now figure out how to pull API data based on ID

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,48 +1,31 @@
 $(document).ready(function () {
     //Variables??
-
-
-
     var topics = ["Surprise","Laughter","Sad","OMG","Cmon man","Seinfeld","Curb Your Enthusiasm","Will Ferrell",
         "Dave Chappelle","Homer" ];
 
-
     //createButtons - Creates buttons via Jquery to HTML from topics array.
     function createButtons() {
-
         for (var i = 0; i < topics.length; i++) {
-            var buttons = $('<button id="giphyButton">'+ topics[i] + '</button>')
-            buttons.appendTo('#reactionButtons');
+            var buttonId = topics[i].toString();
+            var buttonGif = $('<button class="giphyButton" id="giphyButtonId">'+ topics[i] + '</button>');
+            buttonGif.appendTo('#reactionButtons');
+            jQuery(buttonGif).attr("id",topics[i].toString());
 
             //Giphy API
             var searchGiphy = topics[i].toString();
-            console.log(searchGiphy);
-            var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchGiphy + "&api_key=HzjdHhnihCZoCBnpSpRF4JPahUjiUMFu&limit=10");
-            xhr.done(function(data) { console.log("success got data", data); });
-            // console.log(xhr);
+            // console.log(searchGiphy);
+            var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchGiphy + "&api_key=HzjdHhnihCZoCBnpSpRF4JPahUjiUMFu&limit=5");
+            xhr.done(function(data) { console.log("success got data", data);
+
+            });
 
         }
-
-
-
-        $('#reactionButtons').click(function () {
-
-            if(this.id == 'reactionButtons') {
-                // alert('test successful');
-                // $.ajax({
-                //     url: xhr,
-                //     method: "GET"
-                // }).done(function (response) {
-                //     var reactResponse = response.data;
-                //     console.log(reactResponse);
-                // });
-
-                console.log(xhr);
-
-            };
+        $(".giphyButton").click(function () {
+            console.log((jQuery(this).attr('id')));
         });
-
     };
+
+
 
 
 
